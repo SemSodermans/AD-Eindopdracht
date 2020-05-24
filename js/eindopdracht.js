6f7b3ff44928b4d9fe8388d15c53d3afd201b60c
Sem Sodermans <446862@student.fontys.nl>
2020-05-24 22:58:38 +0200

    moving cube
    3d kubus laten zien die roteert, hierna proberen om beweging te verkrijgen via wasd of de pijltjestoetsen.

---
+++ js/eindopdracht.js
@@ -0,0 +1,46 @@
+var scene, camera, renderer, mesh;
+
+var keyboard = {};
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    mesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
+    );
+
+    scene.add(mesh);
+
+    camera.position.z = 5;
+    camera.lookAt(mesh.position);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+}
+
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+window.onload = init;
