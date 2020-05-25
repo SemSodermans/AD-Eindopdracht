4e6e3eb6e7bcb22faab858a88b3cca9fac45b17c
Sem Sodermans <446862@student.fontys.nl>
2020-05-25 21:09:37 +0200

    beweging en camera movement
    wasd beweging verfijnd en de mogelijkheid toegevoegd om de camera te draaien, ik ben nu nog aan het kijken om dit met de muis mogelijk te maken maar ik krijg PointerLock niet aan de praat...

--- js/eindopdracht.js
+++ js/eindopdracht.js
@@ -2,26 +2,29 @@ var scene, camera, renderer, mesh;
 var meshFloor;
 
 var keyboard = {};
-var player = { height: 1.8 };
+var player = { height: 1.8, speed: 0.2, turnSpeed: Math.PI * 0.02 };
 
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     mesh = new THREE.Mesh(
-        new THREE.SphereGeometry(1, 20, 20),
-        new THREE.MeshBasicMaterial({ color: 0x432BD7, wireframe: true })
+        new THREE.SphereGeometry(1, 50, 50),
+        new THREE.MeshBasicMaterial({ color: 0x432BD7, wireframe: false })
 
     );
 
+    mesh.position.y += 1;
     scene.add(mesh);
 
     meshFloor = new THREE.Mesh(
-        new THREE.PlaneGeometry(10, 10, 10, 10),
-        new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false })
+        new THREE.PlaneGeometry(20, 10, 10, 10),
+        new THREE.MeshBasicMaterial({ color: 0xFB6D6D, wireframe: false })
     );
     meshFloor.rotation.x -= Math.PI / 2;
     scene.add(meshFloor);
+
+
 
     camera.position.set(0, player.height, -5);
     camera.lookAt(new THREE.Vector3(0, player.height, 0));
@@ -39,12 +42,36 @@ function animate() {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
 
+    if (keyboard[87]) {
+        camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+        camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+
+    }
+
+    if (keyboard[83]) {
+        camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+
+    }
+
+    if (keyboard[65]) {
+        camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y + Math.PI / 2) * player.speed;
+
+    }
+
+    if (keyboard[68]) {
+        camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * player.speed;
+        camera.position.z += -Math.cos(camera.rotation.y - Math.PI / 2) * player.speed;
+
+    }
+
 
     if (keyboard[37]) {
-        camera.rotation.y -= Math.PI * 0.01;
+        camera.rotation.y -= player.turnSpeed;
     }
     if (keyboard[39]) {
-        camera.rotation.y += Math.PI * 0.01;
+        camera.rotation.y += player.turnSpeed;
     }
 
     renderer.render(scene, camera);
